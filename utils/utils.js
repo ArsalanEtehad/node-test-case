b30a8d227a79b3afbedd67395123338a7e6adf79
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-03 14:15:35 +1100

    assertion tests for async functions

--- utils/utils.js
+++ utils/utils.js
@@ -1,6 +1,19 @@
 module.exports.add = (a,b)=>a+b
+
+module.exports.asyncAdd = (a, b, callback)=>{
+  setTimeout(()=>{
+              callback(a+b)
+            },1000)//delays more than 2 seconds would cause tests to fail, As Mocha failes any delay longer than 2000 ms.
+      }
+
 module.exports.sub = (a,b)=>a-b
 module.exports.square = (a)=>a*a
+module.exports.asyncSquare= (a,callback)=>{
+  setTimeout(()=>{
+    callback(a*a)
+  },1000)
+}
+
 module.exports.setName= (user, fullname)=>{
   var name = fullname.split(' ')
   user.firstname = name[0]
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,11 +3,17 @@
 const utils = require('./utils')
 const expect = require('expect')
 
-
-
 it('should add two numbers', ()=>{
   var res = utils.add(33,11)
   expect(res).toBe(44).toBeA('number')
+})
+
+//use done() when testing async Functions
+it('should asynchronously add two numbers', (done)=>{ //(done()) notifies Mocha that this is a async func and has to wait until it gets DONE
+  utils.asyncAdd(4,3,(sum)=>{
+    expect(sum).toBe(7).toBeA('number')
+    done() //result:   ✓ should add asynchronously two numbers (1003ms)
+  })
 })
 
 it('should subtract two numbers',()=>{
@@ -15,9 +21,16 @@ it('should subtract two numbers',()=>{
   expect(res).toBe(13).toBeA('number')
 })
 
-it('should square a number',()=>{
+it('should square the number',()=>{
   var res = utils.square(5)
   expect(res).toBe(25).toBeA('number')
+})
+
+it('should asynchronously square the number',(done)=>{
+  utils.asyncSquare(3,(res)=>{
+    expect(res).toBe(9).toBeA('number')
+    done()
+  })
 })
 
 it('should expect some values', ()=>{
