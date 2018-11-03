7c054991b0bd05f76551b078af540e739602e621
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-03 13:50:29 +1100

    more tests using mocha and expect (MJACKSON/EXPECT)

--- utils/utils.js
+++ utils/utils.js
@@ -1,3 +1,9 @@
 module.exports.add = (a,b)=>a+b
 module.exports.sub = (a,b)=>a-b
 module.exports.square = (a)=>a*a
+module.exports.setName= (user, fullname)=>{
+  var name = fullname.split(' ')
+  user.firstname = name[0]
+  user.lastname = name[1]
+  return user
+}
--- utils/utils.test.js
+++ utils/utils.test.js
@@ -8,23 +8,26 @@ const expect = require('expect')
 it('should add two numbers', ()=>{
   var res = utils.add(33,11)
   expect(res).toBe(44).toBeA('number')
-  // if(res !== 44){
-  //   throw new Error(`Expected 44, but got ${res}`)
-  // }
 })
 
 it('should subtract two numbers',()=>{
   var res = utils.sub(33,20)
   expect(res).toBe(13).toBeA('number')
-  // if (res !== 13){
-  //   throw new Error(`Expected 13, but got ${res}`)
-  // }
 })
 
 it('should square a number',()=>{
   var res = utils.square(5)
   expect(res).toBe(25).toBeA('number')
-  // if (res !== 25){
-  //   throw new Error(`Expected 25, but got ${res}`)
-  // }
+})
+
+it('should expect some values', ()=>{
+  expect(12).toNotBe(14)
+})
+
+
+it('should set firstname and lastname',()=>{
+  var user ={}
+  user = utils.setName(user, 'Arsalan Ettehadolhagh');
+  expect(user.firstname).toEqual('Arsalan');
+  expect(user.lastname).toEqual('Ettehadolhagh')
 })
