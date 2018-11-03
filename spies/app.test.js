9bdcb5c2357c94b4451408aeafbc897f95bd40ad
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-04 00:18:36 +1100

    s

--- spies/app.test.js
+++ spies/app.test.js
@@ -1,5 +1,7 @@
 const expect = require('expect')
+const rewire = require('rewire')
 
+var app = rewire('./app')  //rewire instead of require
 
 it('should call spy', ()=>{
   var spy = expect.createSpy()
