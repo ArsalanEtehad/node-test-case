f907363b1cfb597d7f1097f301e528e440b04a54
Arsalan <z5163778@ad.unsw.edu.au>
2018-11-03 14:29:22 +1100

    Setting up express server app to make tests for it

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -3,11 +3,12 @@
 const utils = require('./utils')
 const expect = require('expect')
 
+//=======================================
 it('should add two numbers', ()=>{
   var res = utils.add(33,11)
   expect(res).toBe(44).toBeA('number')
 })
-
+//---------------------------------------
 //use done() when testing async Functions
 it('should asynchronously add two numbers', (done)=>{ //(done()) notifies Mocha that this is a async func and has to wait until it gets DONE
   utils.asyncAdd(4,3,(sum)=>{
@@ -15,28 +16,28 @@ it('should asynchronously add two numbers', (done)=>{ //(done()) notifies Mocha 
     done() //result:   ✓ should add asynchronously two numbers (1003ms)
   })
 })
-
+//---------------------------------------
 it('should subtract two numbers',()=>{
   var res = utils.sub(33,20)
   expect(res).toBe(13).toBeA('number')
 })
-
+//---------------------------------------
 it('should square the number',()=>{
   var res = utils.square(5)
   expect(res).toBe(25).toBeA('number')
 })
-
+//---------------------------------------
 it('should asynchronously square the number',(done)=>{
   utils.asyncSquare(3,(res)=>{
     expect(res).toBe(9).toBeA('number')
     done()
   })
 })
-
+//---------------------------------------
 it('should expect some values', ()=>{
   expect(12).toNotBe(14)
 })
-
+//---------------------------------------
 
 it('should set firstname and lastname',()=>{
   var user ={}
@@ -44,3 +45,4 @@ it('should set firstname and lastname',()=>{
   expect(user.firstname).toEqual('Arsalan');
   expect(user.lastname).toEqual('Ettehadolhagh')
 })
+//---------------------------------------
